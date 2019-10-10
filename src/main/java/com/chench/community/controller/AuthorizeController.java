d6adf835763bef9a8e7cea5c99268f99c808ee43
chench <907531870@qq.com>
2019-10-10 21:27:44 +0800

    1

--- src/main/java/com/chench/community/controller/AuthorizeController.java
+++ src/main/java/com/chench/community/controller/AuthorizeController.java
@@ -2,12 +2,18 @@ package com.chench.community.controller;
 
 import com.chench.community.dto.AccessTokenDTO;
 import com.chench.community.dto.GithubUser;
+import com.chench.community.mapper.UserMapper;
+import com.chench.community.model.User;
 import com.chench.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -24,9 +30,14 @@ public class AuthorizeController {
     @Value("${github.redirect.url}")
     private String redirectUrl;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletResponse response
+                             ){
         AccessTokenDTO accessTokenDTO=new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_url(redirectUrl);
@@ -34,8 +45,22 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         accessTokenDTO.setClient_secret(clientSecret);
         String accseeToken=githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user=githubProvider.getUser(accseeToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser=githubProvider.getUser(accseeToken);
+        if(githubUser!=null){
+            User user=new User();
+            String token=UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登录成功,写cookie 和 session
+            response.addCookie(new Cookie("token",token));
+            return "redirect:/";
+        }else{
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
--- src/main/java/com/chench/community/controller/IndexController.java
+++ src/main/java/com/chench/community/controller/IndexController.java
@@ -1,14 +1,36 @@
 package com.chench.community.controller;
 
+import com.chench.community.mapper.UserMapper;
+import com.chench.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if(cookies==null){
+            return "index";
+        }
+        for(Cookie cookie:cookies){
+            if(cookie.getName().equals("token")){
+                String token=cookie.getValue();
+                User user=userMapper.findByToken(token);
+                if(user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 
